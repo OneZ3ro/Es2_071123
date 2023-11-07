dd98f57666974d8164af4e45065bf3fee966ef9e
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-07 15:39:31 +0100

    Aggiunti Controlli per le classi

---
+++ src/main/java/angelomoreno/Es2_071123/controllers/AutoreController.java
@@ -0,0 +1,9 @@
+package angelomoreno.Es2_071123.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autore")
+public class AutoreController {
+}
---
+++ src/main/java/angelomoreno/Es2_071123/controllers/BlogController.java
@@ -0,0 +1,10 @@
+package angelomoreno.Es2_071123.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+    
+}
