3006db06592100517e10180c36d06789a611a3cc
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-07 15:51:25 +0100

    Aggiunto prima funzione blog

--- src/main/java/angelomoreno/Es2_071123/controllers/BlogController.java
+++ src/main/java/angelomoreno/Es2_071123/controllers/BlogController.java
@@ -1,10 +1,20 @@
 package angelomoreno.Es2_071123.controllers;
 
+import angelomoreno.Es2_071123.entities.Blog;
+import angelomoreno.Es2_071123.service.BlogService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/blog")
 public class BlogController {
+    private BlogService blogService;
 
+    @GetMapping("")
+    public List<Blog> getBlogs() {
+        return blogService.getBlogs();
+    }
 }
--- src/main/java/angelomoreno/Es2_071123/service/BlogService.java
+++ src/main/java/angelomoreno/Es2_071123/service/BlogService.java
@@ -1,4 +1,22 @@
 package angelomoreno.Es2_071123.service;
 
+import angelomoreno.Es2_071123.entities.Blog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class BlogService {
+    private List<Blog> blogs = new ArrayList<>();
+
+    public Blog save(Blog body) {
+        Random rndm = new Random();
+        body.setId(rndm.nextLong(1, 100000L));
+        this.blogs.add(body);
+        return body;
+    }
+
+    public List<Blog> getBlogs() {
+        return this.blogs;
+    }
 }
