7568517c816e9022c48318c973693d208355e6cc
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-07 16:57:48 +0100

    Create funzioni per classi autore

--- src/main/java/angelomoreno/Es2_071123/controllers/AutoreController.java
+++ src/main/java/angelomoreno/Es2_071123/controllers/AutoreController.java
@@ -1,9 +1,43 @@
 package angelomoreno.Es2_071123.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import angelomoreno.Es2_071123.entities.Autore;
+import angelomoreno.Es2_071123.service.AutoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/autore")
 public class AutoreController {
+    @Autowired
+    private AutoreService autoreService;
+
+    @GetMapping("")
+    public List<Autore> getAutores() {
+        return autoreService.getAutores();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Autore saveAutore(@RequestBody Autore body){
+        return autoreService.save(body);
+    }
+
+    @GetMapping("/{id}")
+    public Autore findById(@PathVariable long id){
+        return autoreService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Autore modifyAutore(@PathVariable long id, @RequestBody Autore body){
+        return autoreService.modifyAutore(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAutore(@PathVariable long id) {
+        autoreService.deleteAutore(id);
+    }
 }
--- src/main/java/angelomoreno/Es2_071123/service/AutoreService.java
+++ src/main/java/angelomoreno/Es2_071123/service/AutoreService.java
@@ -1,4 +1,66 @@
 package angelomoreno.Es2_071123.service;
 
+import angelomoreno.Es2_071123.entities.Autore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+
 public class AutoreService {
+    private List<Autore> autori = new ArrayList<>();
+
+    public Autore save(Autore body) {
+        Random rndm = new Random();
+        body.setId(rndm.nextLong(1, 100000L));
+        this.autori.add(body);
+        return body;
+    }
+
+    public List<Autore> getAutores() {
+        return this.autori;
+    }
+
+    public Autore findById(long id) {
+        Autore appAutore = new Autore();
+        for (Autore autore : this.autori) {
+            if (autore.getId() == id) {
+                appAutore = autore;
+            }
+        }
+        if (appAutore == null) {
+            throw new RuntimeException("Autore non trovato");
+        } else {
+            return appAutore;
+        }
+    }
+
+    public Autore modifyAutore(long id, Autore body) {
+        Autore app = null;
+        for (Autore blog : this.autori) {
+            if (blog.getId() == id) {
+                app = blog;
+                app.setId(id);
+                app.setNome(body.getNome());
+                app.setCognome(body.getCognome());
+                app.setEmail(body.getEmail());
+                app.setDataDiNascita(body.getDataDiNascita());
+            }
+        }
+        if (app == null) {
+            throw new RuntimeException("Autore non trovato");
+        } else {
+            return app;
+        }
+    }
+
+    public void deleteAutore(long id) {
+        ListIterator<Autore> iterator = this.autori.listIterator();
+        while (iterator.hasNext()) {
+            Autore current = iterator.next();
+            if (current.getId() == id){
+                iterator.remove();
+            }
+        }
+    }
 }
