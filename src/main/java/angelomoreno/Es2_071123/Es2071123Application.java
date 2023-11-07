4bce2ade7f93c3c37666db04a80633294306bea1
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-07 15:08:34 +0100

    Creato progetto

---
+++ src/main/java/angelomoreno/Es2_071123/Es2071123Application.java
@@ -0,0 +1,13 @@
+package angelomoreno.Es2_071123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es2071123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es2071123Application.class, args);
+		System.out.println("Hello World");
+	}
+}
